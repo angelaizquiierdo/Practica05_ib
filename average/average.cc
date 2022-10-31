e379fddead83019027e453963c1fd6c1ff111339
usuario <alu0101480442@ull.edu.es>
2022-10-31 20:06:13 +0000

    subida

--- average/average.cc
+++ average/average.cc
@@ -1,15 +1,24 @@
+ /**
+  * Universidad de La Laguna
+  * Escuela Superior de Ingeniería y Tecnología
+  * Grado en Ingeniería Informática
+  * Informática Básica 2022-2023
+  *
+  * @file sum_three_integal.cc
+  * @author Ángela Zhouling Izquierdo Padrón
+  * @date 26 october 2022
+  * @brief Write a program that reads three numbers and prints their sum.
+  */
+
 #include<iostream>
 #include<iomanip>
 int main() {
-  double valor;
-  double suma{0};
-  int intinerador{0};
+  double valor, suma{0.0}, intinerador{0.0};
   while(std::cin >> valor) {
     suma += valor;
     intinerador++;
 }
   double resultado = suma / intinerador;
-  std::cout <<  std::setprecision(2) << resultado << std::endl;
+  std::cout << std::fixed << std::setprecision(2) << resultado << std::endl;
 return 0;
 }
-
